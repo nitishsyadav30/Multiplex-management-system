f91a351274ddf0abe62e6be85f19039bcc257d05
nitishsyadav30 <nitishsyadav30@gmail.com>
2014-02-17 17:39:01 +0530

    third Commit

--- includes/installscript.php
+++ includes/installscript.php
@@ -1,7 +1,7 @@
 <?php 
     include '../config.php';
     //include BASE_PATH .'/includes/connection.php';
-    $prefix="multiplex";
+    $prefix="multiplex_";
     $db_con=  mysqli_connect("localhost","multiplex","multiplex123");
     if(mysqli_connect_errno())
     {
@@ -20,5 +20,8 @@
     $add_multiplex_table="create table".$prefix."add_multiplex(mul_id varchar(10) primary key,mul_name varchar(12),mul_city varchar(10),mul_area varchar(10),mul_addr varchar(100),mul_screens int(10)";
     $add_screens_table="create table".$prefix."add_screen(screen_id varchar(6) primary key,screen_no int(2),mul_id varchar(10),screen_strength int(3),balcony_seats int(3),dc_seats int(3),foreign key(mul_id) references .'$prefix'.add_multiplex(mul_id))";
     
+    $add_shows_table="create table". $prefix."add_show(show_id int(5) primary key,screen_id varchar(6),mul_id varchar(10),show_date datetime,show_time datetime,'foreign key(mul_id) references' .'$prefix'.'add_multiplex(mul_id)','foreign key(screen_id) references' .$prefix.'add_screen(screen_id)')";
+    
+    $add_booking_table="create table".$prefix."booking(booking_id int(5),movie_id varchar(5),user-email varchar(30),show_id int(5),screen_id varchar(6),movie_id varchar(5),no_of_seats int(2),seat_no int(2))";
     ?>
 
