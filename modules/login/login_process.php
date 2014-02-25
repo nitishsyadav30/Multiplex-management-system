f7a97b6cb178d7baf752b38c3ef1764200b015a0
nitishsyadav30 <nitishsyadav30@gmail.com>
2014-02-25 16:20:14 +0530

    Commit

--- modules/login/login_process.php
+++ modules/login/login_process.php
@@ -1,47 +1,46 @@
 <?php
-  session_start();
- 
+
+session_start();
+
 /* @var $_POST type */
- $email= $_POST['email'];
- $password= $_POST['password'];
- 
-  require_once  '../../config.php';
-  require_once  BASE_PATH. '/includes/connection_final.php';
-  
- $select="select user_email,pass,user_role,fname,lname from multiplex_register where user_email like '$email' AND pass like '$password' ";
- 
- $queryresult = mysqli_query($con,$select) or die(mysqli_error($con));
- 
- while($row_result= mysqli_fetch_array($queryresult))
-      {
-         $userole=$row_result['user_role'];
-         
-         if($row_result['user_role']==1)
-             {
-              $_SESSION['fname']=$row_result['fname'];
-              $_SESSION['lname']=$row_result['lname'];
-              $_SESSION['CurrentUser']=$email;
-               $_SESSION['user_role']=$userole;
-               //$_SESSION['id']=$row_result['user_role'];
-               //include BASE_PATH. '/adminHome.php';
-               Header("Location: $address/index.php"); 
-              
-             }
-             else if($row_result['user_email']=="$email" && $row_result['pass']=="$password")
-                 {
-                 $_SESSION['fname']=$row_result['fname'];
-              $_SESSION['lname']=$row_result['lname'];
-                 $_SESSION['CurrentUser']=$email;
-                  $_SESSION['user_role']=$userole;
-               
-                  Header("Location: $address/index.php"); 
-                 }
-                 else 
-                     { 
-                    
-                      
-                      }
-                 
-      }
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+require_once '../../config.php';
+require_once BASE_PATH . '/includes/connection_final.php';
+
+$select = "select user_email,pass,user_role,fname,lname from multiplex_register where user_email like '$email' AND pass like '$password' ";
+
+$queryresult = mysqli_query($con, $select) or die(mysqli_error($con));
+
+while ($row_result = mysqli_fetch_array($queryresult)) {
+    $userole = $row_result['user_role'];
+
+    if ($row_result['user_role'] == 1) {
+        $_SESSION['fname'] = $row_result['fname'];
+        $_SESSION['lname'] = $row_result['lname'];
+        $_SESSION['CurrentUser'] = $email;
+        $_SESSION['user_role'] = $userole;
+        //$_SESSION['id']=$row_result['user_role'];
+        //include BASE_PATH. '/adminHome.php';
+        Header("Location: $address/index.php");
+    } else if ($row_result['user_email'] == "$email" && $row_result['pass'] == "$password") {
+        $_SESSION['fname'] = $row_result['fname'];
+        $_SESSION['lname'] = $row_result['lname'];
+        $_SESSION['CurrentUser'] = $email;
+        $_SESSION['user_role'] = $userole;
+
+        Header("Location: $address/index.php");
+    } else 
+        {
+        Header("Location: $address/index.php");
+       echo "<div align = 'center' class = 'centerdiv' style = 'width: 1000px; '>";
+       echo "<h2>You have been Logged Out..!!!</h2>";
+
+       echo "</div>";
+    }
+    
+    
+}
 ?>
 
